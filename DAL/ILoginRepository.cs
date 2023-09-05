1bc2b765880874e492bab5261e6180ea7279ab77
mathiasck <mathias.ck2000@gmail.com>
2023-09-05 19:04:43 +0200

    Store user id in session

--- DAL/ILoginRepository.cs
+++ DAL/ILoginRepository.cs
@@ -6,6 +6,7 @@ namespace Discussions.DAL
 	public interface ILoginRepository
 	{
         public void SetSessionEmail(User user);
+        public Task<User?> FetchUser(string email);
 
     }
 }
--- DAL/LoginRepository.cs
+++ DAL/LoginRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Discussions.Controllers;
 using Discussions.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discussions.DAL
 {
@@ -8,28 +9,35 @@ namespace Discussions.DAL
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ILogger<HomeController> _logger;
+        private readonly DB _db;
 
         public LoginRepository(IHttpContextAccessor httpContextAccessor, ILogger<HomeController> logger, DB db)
         {
             _httpContextAccessor = httpContextAccessor;
             _logger = logger;
+            _db = db;
         }
 
-        public void SetSessionEmail(User user)
+        public async void SetSessionEmail(User user)
         {
             try
             {
-                Guid uuid = Guid.NewGuid();
+                var userExists = await FetchUser(user.Email);
+                string sessionId = userExists?.Id ?? Guid.NewGuid().ToString();
 
-                _httpContextAccessor.HttpContext.Session.SetString("UserId", uuid.ToString());
-                _httpContextAccessor.HttpContext.Session.SetString("UserEmail", user.Email);
-                _logger.LogInformation("Sucsessfully set Session value UserEmail to: {userEmail}", user.Email);
+                _httpContextAccessor.HttpContext?.Session.SetString("UserId", sessionId);
+                _httpContextAccessor.HttpContext?.Session.SetString("UserEmail", user.Email);
+                _logger.LogInformation("Sucsessfully set Session value 'UserEmail' to: {userEmail} and value 'UserId' to: {userId}", user.Email, sessionId);
             } catch (Exception e)
             {
                 _logger.LogError("There was an error setting Session value UserEmail: {e}", e.Message);
             }
         }
- 
-	}
+        public async Task<User?> FetchUser(string email)
+        {
+            return await _db.Users.FirstOrDefaultAsync(u => u.Email == email) ?? null;
+
+        }
+}
 }
 
