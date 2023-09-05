6ed0f919224900bb9448d14de519151d9e016f4d
mathiasck <mathias.ck2000@gmail.com>
2023-09-05 10:21:00 +0200

    Add Discussion model

---
+++ Models/Discussion.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Discussions.Models
+{
+	public class Discussion
+	{
+		
+		public int Id { get; set; }
+		[StringLength(50)]
+        public string Header { get; set; } = string.Empty;
+        [StringLength(100)]
+        public string Body { get; set; } = string.Empty;
+        public User Author { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+
+		public Discussion(int id, string header, string body, User author, DateTime created, DateTime updated)
+		{
+			Id = id;
+			Header = header;
+			Body = body;
+			Author = author;
+			Created = created;
+			Updated = updated;
+		}
+	}
+}
+
