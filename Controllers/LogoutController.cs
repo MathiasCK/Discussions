d8ea737e066f7207a3f24d9e98155a428ade2cc1
mathiasck <mathias.ck2000@gmail.com>
2023-09-06 12:38:15 +0200

    Add logout route

---
+++ Controllers/LogoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Discussions.Controllers
+{
+    [ServiceFilter(typeof(UserSessionFiler))]
+    public class LogoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Confirm()
+        {
+
+            HttpContext.Session.Remove("UserId");
+            HttpContext.Session.Remove("UserEmail");
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
+
